fadb30bce51470a6e80ca994147a306140128a0a
hyrdbyrd <IRuss23@yandex.ru>
2019-02-13 00:12:39 +0300

    fix: test - startOf.test.js date fix

--- test/startOf.test.js
+++ test/startOf.test.js
@@ -6,6 +6,6 @@ dayjs.extend(utcPlugin)
 test('start of / end of', () => {
   const a = dayjs.utc('2019-01-03T21:30:00.000Z')
 
-  expect(a.clone().startOf('day').toISOString()).toBe('2019-01-03T00:00:00.000Z')
-  expect(a.clone().endOf('day').toISOString()).toBe('2019-01-03T23:59:59.999Z')
+  expect(a.clone().startOf('day').toISOString()).toBe('2019-01-04T00:00:00.000Z')
+  expect(a.clone().endOf('day').toISOString()).toBe('2019-01-04T23:59:59.999Z')
 })
